50e12c12048f1833b8e074bcc6889f0ace2b9fbd
dalenysivilay <dalenysivilay@gmail.com>
2022-01-05 15:46:47 -0600

    build pushed

--- lib/screens/google_map_page.dart
+++ lib/screens/google_map_page.dart
@@ -1,26 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class GoogleMapPage extends StatefulWidget {
+class GoogleMapPage extends StatelessWidget {
   const GoogleMapPage({Key key}) : super(key: key);
-
-  @override
-  _GoogleMapPageState createState() => _GoogleMapPageState();
-}
-
-class _GoogleMapPageState extends State<GoogleMapPage> {
-  Set<Marker> _markers = {};
-
-  void _onMapCreated(GoogleMapController controller) {
-    setState(() {
-      _markers.add(
-        Marker(
-          markerId: MarkerId('id-1'),
-          position: LatLng(35.07396943121907, -89.93218569969048),
-        ),
-      );
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +11,6 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
         title: Text("Location"),
       ),
       body: GoogleMap(
-        onMapCreated: _onMapCreated,
         initialCameraPosition: CameraPosition(
           target: LatLng(35.07396943121907, -89.93218569969048),
           zoom: 15,
